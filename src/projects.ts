3f7012044ecbba1bb74c7f64c87c97ccc4e6143c
bryan <bryan.deng002@gmail.com>
2022-10-28 10:56:15 -0400

    simplify and update projects page

---
+++ src/projects.ts
@@ -0,0 +1,51 @@
+type Project = {
+    name: string;
+    url: string;
+    tools: string[];
+    project_description: string;
+    image_path: string;
+    image_description: string;
+}
+
+export const projects: Project[] = [
+    {
+        name: "RayJL",
+        url: "https://github.com/Blackgaurd/RayJL",
+        tools: ["Julia", "Python"],
+        project_description: "Fully functional, optimized and multithreaded ray-tracing library built from scratch. Support for poly-mesh files, mirrors, refraction, colored lights and more.",
+        image_path: "images/rayjl.png",
+        image_description: "Torus triangle mesh with green and red point lights casting shadows on the ground. Rendered in ~5 seconds on 8 threads.",
+    },
+    {
+        name: "GDT",
+        url: "https://github.com/Blackgaurd/GDT",
+        tools: ["Python"],
+        project_description: "Genetic Decision Tree. An algorithm that mimicks biological evolution in order to construct optimized decision trees for data classification.",
+        image_path: "images/gdt.png",
+        image_description: "Graphs demonstrating how the population size of the algorithm affects factors such as training time, test accuracy and how fast the algorithm converges.",
+    },
+    {
+        name: "AP Calc BC Notes",
+        url: "https://github.com/Blackgaurd/AP-CalcBC-Notes",
+        tools: ["LaTeX"],
+        project_description: "Complete set of notes for the AP Calculus BC course (100+ pages). Includes all units, as well as practice problems and solutions explained in rigurous detail.",
+        image_path: "images/calc.png",
+        image_description: "A screenshot of the notes (Taylor Series section).",
+    },
+    {
+        name: "TI-84 Wordle",
+        url: "https://github.com/Blackgaurd/TI84-Wordle",
+        tools: ["C", "Assembly"],
+        project_description: "Wordle clone written using the C/CE Toolchain and compiled for the ez80 processor. Comes with the (almost) complete word list from the original Wordle.",
+        image_path: "images/wordle.jpg",
+        image_description: "A demonstration of the compiled game being played on a TI-84 Plus CE.",
+    },
+    {
+        name: "YourSleep",
+        url: "https://github.com/Blackgaurd/IgnitionHacks2022",
+        tools: ["Svelte", "TailwindCSS", "Typescript", "Java"],
+        project_description: "Personalized circadian cycle calculator that learns from your sleep habits over time. Built for IgnitionHacks 2022.",
+        image_path: "images/yoursleep.png",
+        image_description: "Screenshot from the live demo of the project.",
+    }
+]
--- src/routes/ProjectsPage.svelte
+++ src/routes/ProjectsPage.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import Project from "./Project.svelte";
+
+	import { projects } from "../projects";
 </script>
 
 <div id="projects" class="px-10 py-20 sm:px-14 lg:px-28">
@@ -11,54 +13,12 @@
 	</div>
 	<!--using flexbox bc grid cant center last row-->
 	<div class="flex flex-wrap justify-center">
-		<!--TODO: add how many objects were in the torus render-->
-		<div class="my-1 3xl:w-1/2">
-            <div class="3xl:mx-1">
-                <Project
-				name="RayJL"
-				url="https://github.com/Blackgaurd/RayJL"
-				image_path="images/rayjl.png"
-				image_description="Torus triangle mesh with green and red point lights casting shadows on the ground. Rendered in ~5 seconds on 8 threads."
-				project_description="Fully functional, optimized and multithreaded ray-tracing library built from scratch. Support for poly-mesh files, mirrors, refraction, colored lights and more."
-				tools={["Julia", "Python"]}
-                />
-            </div>
-		</div>
-		<div class="my-1 3xl:w-1/2">
-            <div class="3xl:mx-1">
-                <Project
-				name="GDT"
-				url="https://github.com/Blackgaurd/GDT"
-				image_path="images/gdt.png"
-				image_description="Graphs demonstrating how the population size of the algorithm affects factors such as training time, test accuracy and how fast the algorithm converges."
-				project_description="Genetic Decision Tree. An algorithm that mimicks biological evolution in order to construct optimized decision trees for data classification."
-				tools={["Python"]}
-                />
-            </div>
-		</div>
-		<div class="my-1 3xl:w-1/2">
-            <div class="3xl:mx-1">
-                <Project
-				name="AP Calc BC Notes"
-				url="https://github.com/Blackgaurd/AP-CalcBC-Notes"
-				image_path="images/calc.png"
-				image_description="A screenshot of the notes (Taylor Series section)."
-				project_description="Complete set of notes for the AP Calculus BC course (100+ pages). Includes all units, as well as practice problems and solutions explained in rigurous detail."
-				tools={["LaTeX"]}
-                />
-            </div>
-		</div>
-		<div class="my-1 3xl:w-1/2">
-            <div class="3xl:mx-1">
-                <Project
-				name="TI-84 Wordle"
-				url="https://github.com/Blackgaurd/TI84-Wordle"
-				image_path="images/wordle.jpg"
-				image_description="A demonstration of the compiled game being played on a TI-84 Plus CE."
-				project_description="Wordle clone written using the C/CE Toolchain and compiled for the ez80 processor. Comes with the (almost) complete word list from the original Wordle."
-				tools={["C", "Assembly"]}
-                />
-            </div>
-		</div>
+		{#each projects as { name, url, tools, project_description, image_path, image_description }}
+			<div class="my-1 3xl:w-1/2">
+				<div class="3xl:mx-1">
+					<Project {name} {url} {tools} {project_description} {image_path} {image_description} />
+				</div>
+			</div>
+		{/each}
 	</div>
 </div>
